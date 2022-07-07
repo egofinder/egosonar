d612f20aa830f18b9b8539a7f5d5adb7c3c3e874
baekhak JEon <baekhakjeon@gmail.com>
2022-07-07 11:19:47 -0700

    master

--- database/migrations/2022_06_30_211357_create_listings_table.php
+++ database/migrations/2022_06_30_211357_create_listings_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
-            // $table->string('logo')->nullable();
             $table->string('tags');
             $table->string('company');
             $table->string('location');
